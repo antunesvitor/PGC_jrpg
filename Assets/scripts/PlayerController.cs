27dae591684d98db31aed81a264bcfa7dbcf02f3
Vitor Guilherme Antunes <vguilhermeantunes@gmail.com>
2022-09-27 09:55:39 -0300

    adicionado animações de ataque

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using static Assets.scripts.Utils.Constantes;
 
 public class PlayerController : MonoBehaviour
-{   
+{
     // Start is called before the first frame update
     public CharacterController controller;
     public Transform cam;
@@ -24,17 +24,15 @@ public class PlayerController : MonoBehaviour
     public float runningModifier = 1.0f;
 
 
-    
+
     public bool isExecutingAction = false;
 
     private bool EstahOcioso { get { return this.EstadoAtual == ESTADO_JOGADOR.OCIOSO; } }
-
     private bool EstahPreparandoAcao { get { return this.EstadoAtual == ESTADO_JOGADOR.PREPARANDO; } }
-
     private bool EstahExecutandoAcao { get { return this.EstadoAtual == ESTADO_JOGADOR.EXECUTANDO; } }
 
     public float _tempoDecorridoPreparandoAcao;
-    public float _porcentagemAcao { get { return (this._tempoDecorridoPreparandoAcao / Constantes.SEGUNDOS_PREPARACAO__ATAQUE) * 100; }  }
+    public float _porcentagemAcao { get { return (this._tempoDecorridoPreparandoAcao / Constantes.SEGUNDOS_PREPARACAO__ATAQUE) * 100; } }
 
     public float _tempoDecorridoExecutandoAcao;
 
@@ -70,9 +68,8 @@ public class PlayerController : MonoBehaviour
                 this.EstadoAtual = ESTADO_JOGADOR.OCIOSO;
                 this._tempoDecorridoExecutandoAcao = 0f;
                 this.movimentoTravado = false;
+                animator.SetBool("isAttacking", false);
             }
-            //bloquear movimento (provavelmente diminuir a velocidade pra zero)
-            //começar a executar a animação de ataque
         }
 
         if (EstahPreparandoAcao)
@@ -80,12 +77,13 @@ public class PlayerController : MonoBehaviour
             float tempoDecorridoAtualizado = this._tempoDecorridoPreparandoAcao + Time.deltaTime;
             this._tempoDecorridoPreparandoAcao = Mathf.Clamp(tempoDecorridoAtualizado, 0, Constantes.SEGUNDOS_PREPARACAO__ATAQUE);
 
-            if(this._tempoDecorridoPreparandoAcao >= Constantes.SEGUNDOS_PREPARACAO__ATAQUE)
+            if (this._tempoDecorridoPreparandoAcao >= Constantes.SEGUNDOS_PREPARACAO__ATAQUE)
             {
                 Debug.Log("AÇÃO CARREGADA!");
                 this._tempoDecorridoPreparandoAcao = 0f;
                 this.EstadoAtual = ESTADO_JOGADOR.EXECUTANDO;
                 this.movimentoTravado = true;
+                animator.SetBool("isAttacking", true);
             }
         }
 
@@ -105,54 +103,57 @@ public class PlayerController : MonoBehaviour
 
     void UpdatePosition()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-
-        Vector3 direction = new Vector3(x, 0, y).normalized;
-
-        bool isWalking = animator.GetBool("isWalking");
-        bool isRunning = animator.GetBool("isRunning");
-        bool isPressingForward = direction.magnitude >= .1f;
-        bool isPressingRunning = Input.GetKey(KeyCode.LeftShift);
-
-        //Debug.Log($"direction x: {direction.x} | y: {direction.y} | z: {direction.z}");
-        //Debug.Log($"isWalking: {isWalking} | isPressingForward: {isPressingForward}");
-
-        float runningFactor = isPressingRunning ? this.runningModifier : 1f;
-
-        if (isPressingForward && !movimentoTravado)
+        if (!movimentoTravado)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle , 0f);
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Vertical");
 
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection * _velocidadeBase * runningFactor * Time.deltaTime);
-        }
+            Vector3 direction = new Vector3(x, 0, y).normalized;
 
-        //Começar andar p/ frente quando apertar pra frente
-        if(!isWalking && isPressingForward)
-        {
-            animator.SetBool("isWalking", true);
-        }
+            bool isWalking = animator.GetBool("isWalking");
+            bool isRunning = animator.GetBool("isRunning");
+            bool isPressingForward = direction.magnitude >= .1f;
+            bool isPressingRunning = Input.GetKey(KeyCode.LeftShift);
 
-        //Se tiver andando e pressionar shift trocar pra correndo
-        if(isWalking && isPressingForward && isPressingRunning)
-        {
-            animator.SetBool("isRunning", true);
-        }
+            //Debug.Log($"direction x: {direction.x} | y: {direction.y} | z: {direction.z}");
+            //Debug.Log($"isWalking: {isWalking} | isPressingForward: {isPressingForward}");
 
-        //Parar de correr e passar a andar se parar de apertar shift
-        if(isRunning && isPressingForward && !isPressingRunning)
-        {
-            animator.SetBool("isRunning", false);
-        }
+            float runningFactor = isPressingRunning ? this.runningModifier : 1f;
 
-        //Trocar animação p/ idle quanto parar de apertar W
-        if(isWalking && !isPressingForward)
-        {
-            animator.SetBool("isWalking", false);
-            animator.SetBool("isRunning", false);
+            if (isPressingForward)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDirection * _velocidadeBase * runningFactor * Time.deltaTime);
+            }
+
+            //Começar andar p/ frente quando apertar pra frente
+            if (!isWalking && isPressingForward)
+            {
+                animator.SetBool("isWalking", true);
+            }
+
+            //Se tiver andando e pressionar shift trocar pra correndo
+            if (isWalking && isPressingForward && isPressingRunning)
+            {
+                animator.SetBool("isRunning", true);
+            }
+
+            //Parar de correr e passar a andar se parar de apertar shift
+            if (isRunning && isPressingForward && !isPressingRunning)
+            {
+                animator.SetBool("isRunning", false);
+            }
+
+            //Trocar animação p/ idle quanto parar de apertar W
+            if (isWalking && !isPressingForward)
+            {
+                animator.SetBool("isWalking", false);
+                animator.SetBool("isRunning", false);
+            }
         }
     }
 }
